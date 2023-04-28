9e9744470a45cb9a7db02bbfe5f6ae236ce028f6
Kodisang <br.kodisang@gmail.com>
2023-04-28 09:24:08 -0700

    1000-sort_deck.c

--- deck.h
+++ deck.h
@@ -1,49 +1,50 @@
+#ifndef DECK_H
+#define DECK_H
+
 #include <stdlib.h>
-#include <stdio.h>
-#include "deck.h"
 
-int compare_cards(const card_t *a, const card_t *b) {
-    static const char *values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-    int value_a, value_b;
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
 
-    for (value_a = 0; value_a < 13 && values[value_a] != a->value; ++value_a);
-    for (value_b = 0; value_b < 13 && values[value_b] != b->value; ++value_b);
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
 
-    if (value_a == value_b) {
-        return a->kind - b->kind;
-    } else {
-        return value_a - value_b;
-    }
-}
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
 
-void sort_deck(deck_node_t **deck) {
-    deck_node_t *current = *deck;
-    deck_node_t *next;
-    deck_node_t *sorted = NULL;
-    deck_node_t *temp;
+void sort_deck(deck_node_t **deck);
 
-    while (current != NULL) {
-        next = current->next;
-        if (sorted == NULL || compare_cards(current->card, sorted->card) < 0) {
-            current->next = sorted;
-            if (sorted != NULL) {
-                sorted->prev = current;
-            }
-            sorted = current;
-        } else {
-            temp = sorted;
-            while (temp->next != NULL && compare_cards(current->card, temp->next->card) > 0) {
-                temp = temp->next;
-            }
-            current->next = temp->next;
-            if (temp->next != NULL) {
-                temp->next->prev = current;
-            }
-            temp->next = current;
-            current->prev = temp;
-        }
-        current = next;
-    }
-
-    *deck = sorted;
-}
+#endif /* DECK_H */
